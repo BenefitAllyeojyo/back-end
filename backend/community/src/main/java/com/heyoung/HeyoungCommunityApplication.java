11a982c39072117819e8fd4cd9a6281a24415c74
redblackblossom <physicsdev1029@gmail.com>
2025-08-15 17:28:34 +0900

    [FEAT] 프로젝트 초기설정을 수행했습니다.

---
+++ backend/community/src/main/java/com/heyoung/HeyoungCommunityApplication.java
@@ -0,0 +1,12 @@
+package com.heyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyoungCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyoungCommunityApplication.class, args);
+	}
+}
