4b0a85d640c8c4f906ab2f244153029d5042c3c8
redblackblossom <physicsdev1029@gmail.com>
2025-08-16 20:16:53 +0900

    [TEST] 모든 컨텍스트를 로드하는 테스트 코드 작성" "

---
+++ backend/core-bank/src/test/java/com/heyoung/HeyoungCoreBankApplicationTests.java
@@ -0,0 +1,12 @@
+package com.heyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HeyoungCoreBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
