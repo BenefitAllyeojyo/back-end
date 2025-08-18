b1b109e36ae4b193218455cc279702d3dfdc9f70
code-crafter-kr <sej324@lehigh.edu>
2025-08-18 22:33:58 +0900

    [FEAT] 페이지 렌더링 라우터 수정 및 전역 css 재설정

--- frontend/heyyoung-webapp/src/main.jsx
+++ frontend/heyyoung-webapp/src/main.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from '@/Store/store'
-import AppShell from '@/Components/layout/AppShell'
-import BenefitsHome from '@/Page/BenefitsHome'
-import Payment from '@/Page/Payment'
-import NotFound from '@/Page/NotFound'
+import App from './App'
 import '@/Style/tokens.css'
 import '@/Style/globals.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <AppShell>
-          <Routes>
-            <Route path="/" element={<BenefitsHome />} />
-            <Route path="/pay" element={<Payment />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AppShell>
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 )
