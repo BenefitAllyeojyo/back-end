956e63b3b37f5e826e328d0d765b6179548ce914
Seo Ga Eun <seo323333@naver.com>
2025-08-19 17:10:34 +0900

    [FEAT] payReqText Module 완료

--- frontend/heyyoung-webapp/src/Components/molecules/TextGrp/PayReqTextModule.jsx
+++ frontend/heyyoung-webapp/src/Components/molecules/TextGrp/PayReqTextModule.jsx
@@ -5,12 +5,27 @@ export default function PayTitleModule({
   ShopName = '레드버튼 보드게임',
   money = 1000000,
 }) {
+  //숫자에 천 단위 구분 쉼표 추가하는 함수
+  const formatAmount = (amount) => {
+    if (typeof amount === 'string') {
+      const numericValue = amount.replace(/[^\d]/g, '');
+      return numericValue.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+    if (typeof amount === 'number') {
+      return amount.toLocaleString();
+    }
+    return amount;
+  };
+
+  const formattedAmount = formatAmount(money);
+
   return (
     <div className={styles.payReqTextModule}>
       <div className={styles.subTitle}>{subTitle}</div>
       <div className={styles.contentsContainer}>
         <span className={styles.shopName}>{ShopName}</span> 에서
-        <span className={styles.money}>{money}</span>원을 결제합니다.
+        <br />
+        <span className={styles.money}>{formattedAmount}</span> 원을 결제합니다.
       </div>
     </div>
   );
