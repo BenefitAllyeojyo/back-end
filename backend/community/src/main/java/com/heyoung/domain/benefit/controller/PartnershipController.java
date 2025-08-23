88ce619405714720415f62ca924785350063985c
vkdlxldz <abcde7712a@naver.com>
2025-08-23 17:25:29 +0900

    [feat] swagger api 명세 작성

--- backend/community/src/main/java/com/heyoung/domain/benefit/controller/PartnershipController.java
+++ backend/community/src/main/java/com/heyoung/domain/benefit/controller/PartnershipController.java
@@ -3,10 +3,12 @@ package com.heyoung.domain.benefit.controller;
 import com.heyoung.domain.benefit.dto.PartnershipDto;
 import com.heyoung.domain.benefit.service.PartnershipService;
 import com.heyoung.global.webconfig.MemberId;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 
+@Tag(name="제휴 정보 관련 API", description = "제휴 정보를 조회할 수 있는 API입니다.")
 @RestController
 @RequestMapping("/partnerships")
 public class PartnershipController {
@@ -17,9 +19,9 @@ public class PartnershipController {
         this.partnershipService = partnershipService;
     }
 
+    @Operation(summary="사용자 대학의 제휴 정보를 조회하는 API", description = "사용자 대학의 제휴 정보를 조회하는 API입니다.")
     @GetMapping("/university")
     public List<PartnershipDto> getPartnerships(@MemberId Long memberId) {
         return partnershipService.findPartnerships(memberId);
     }
-
 }
