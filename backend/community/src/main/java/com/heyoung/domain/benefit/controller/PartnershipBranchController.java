845c6f774d813064b0de1b1daf9882abe4c468fa
vkdlxldz <abcde7712a@naver.com>
2025-08-23 18:03:36 +0900

    [feat] swagger api 명세 작성

--- backend/community/src/main/java/com/heyoung/domain/benefit/controller/PartnershipBranchController.java
+++ backend/community/src/main/java/com/heyoung/domain/benefit/controller/PartnershipBranchController.java
@@ -2,6 +2,8 @@ package com.heyoung.domain.benefit.controller;
 
 import com.heyoung.domain.benefit.dto.PartnershipBranchDto;
 import com.heyoung.domain.benefit.service.PartnershipBranchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name="제휴 지점 정보 관련 API", description = "제휴 지점 정보를 조회할 수 있는 API입니다.")
 @RestController
 @RequestMapping("/partnerships")
 public class PartnershipBranchController {
@@ -19,6 +22,7 @@ public class PartnershipBranchController {
         this.partnershipBranchService = partnershipBranchService;
     }
 
+    @Operation(summary="모든 제휴 지점의 정보를 조회하는 API", description = "모든 제휴 지점의 정보를 조회하는 API입니다.")
     @GetMapping("/{partnershipId}/branches")
     public List<PartnershipBranchDto> getAllPartnershipBranches(@PathVariable Long partnershipId) {
         return partnershipBranchService.findAllPartnershipBranches(partnershipId);
