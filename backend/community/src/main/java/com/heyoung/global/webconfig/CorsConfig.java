5f79cafb62ee169c9cc279cba2f1708c71e3e7f7
redblackblossom <physicsdev1029@gmail.com>
2025-08-24 15:51:45 +0900

    [FEAT] 버그 수정

---
+++ backend/community/src/main/java/com/heyoung/global/webconfig/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.heyoung.global.webconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")      // 모든 URL
+					.allowedOrigins("*")    // 모든 도메인 허용
+					.allowedMethods("*")    // 모든 HTTP 메서드 허용
+					.allowedHeaders("*");   // 모든 헤더 허용
+			}
+		};
+	}
+}
--- backend/core-bank/src/main/java/com/heyoung/global/enums/BankCode.java
+++ backend/core-bank/src/main/java/com/heyoung/global/enums/BankCode.java
@@ -4,4 +4,5 @@ package com.heyoung.global.enums;
  * TODO : 추후에 공식적인 bank code 추가 예정.
  */
 public enum BankCode {
+	HI
 }
